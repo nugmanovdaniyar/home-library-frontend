1f60db3ea20be5782d3489cf61ff18ad84a71110
nugmanovdaniyar <daniyar31604@gmail.com>
2020-10-03 21:40:47 +0700

    Revert "Remove old index.html"
    This reverts commit 742cc4c5

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/theme/index.css';
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter } from 'react-router-dom';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-), document.getElementById('root'))
-
-// ReactDOM.render(<App/>, document.getElementById('root'));
-
-document.addEventListener('DOMContentLoaded', () => {
-    const changeBlock = function changeBlock() {
-        const browserHeight = document.documentElement.clientHeight;
-        [].forEach.call(document.querySelectorAll('.app'), (el) => {
-            el.style.height = `${browserHeight}px`;
-        });
-    };
-
-    window.onresize = changeBlock;
-    changeBlock();
-});
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
